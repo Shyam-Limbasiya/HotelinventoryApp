5304b107ecf76f5f51c7cfb117d76b6441536345
Shyam-Limbasiya <shyamlimbasiya7@gmail.com>
2025-02-10 18:25:12 +0530

    Fourth Commit

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -3,8 +3,6 @@ import { Rooms,RoomList } from './rooms';
 import { CommonModule, DatePipe, NgClass, NgFor, NgIf } from '@angular/common';
 import { RoomListComponent } from './room-list/room-list.component';
 import { FormsModule, NgModel } from '@angular/forms';
-import * as bootstrap from 'bootstrap';
-
 
 @Component({
   selector: 'app-rooms',
@@ -21,6 +19,39 @@ export class RoomsComponent {
   hideRooms = false;
   selectedRoom: RoomList |null = null;
   totalRooms!: number;
+  originalRoomList: RoomList[]= [];
+
+  constructor() {
+    this.totalRooms = this.RoomList.length;
+    this.originalRoomList = [...this.RoomList];
+  }
+
+searchRooms(event: Event) {
+  const searchValue = (event.target as HTMLInputElement).value.toLowerCase();
+  
+  if (!searchValue) {
+    this.RoomList = [...this.originalRoomList];  // Reset to original when empty
+    return;
+  }
+
+  this.RoomList = this.originalRoomList.filter(room =>
+    room.roomType.toLowerCase().includes(searchValue) ||
+    room.roomNumber.toString().includes(searchValue) 
+  );
+}
+
+  
+sortAscending: boolean = true; // Track sorting order
+
+sortByPrice() {
+  this.RoomList.sort((a, b) => {
+    return this.sortAscending ? a.price - b.price : b.price - a.price;
+  });
+
+  this.sortAscending = !this.sortAscending; // Toggle sorting order
+}
+
+
 
   newRoom: RoomList = {
     roomNumber: 0,
@@ -31,9 +62,6 @@ export class RoomsComponent {
     checkInTime: new Date(),
     checkOutTime: new Date()
   };
-  
-  
-  
   toggLe() {
     this.hideRooms = !this.hideRooms;
   }
@@ -79,9 +107,7 @@ export class RoomsComponent {
       checkInTime: new Date('2021-01-03'),
       checkOutTime: new Date('2021-01-07')
     },
-    
   ];
- 
   onRoomSelected(room: RoomList): void {
     this.selectedRoom = room;
   }
@@ -104,8 +130,6 @@ export class RoomsComponent {
     this.RoomList = this.RoomList.filter(room => room.roomNumber !== roomNumber);
     this.totalRooms = this.RoomList.length;  // Update totalRooms count
   }
-
-  
   showToastMessage(message: string): void {
     alert(message);
   }  
